9f136d960119099a7c8be96daa7b707ed5715bd6
Sadokefrei <146061095+Sadokefrei@users.noreply.github.com>
2025-04-17 11:04:24 +0200

    Create homeView.vue
    composant

---
+++ homeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="p-4">
+    <MotoGallery />
+  </div>
+</template>
+
+<script>
+import MotoGallery from '@/components/MotoGallery.vue';
+
+export default {
+  components: { MotoGallery }
+};
+</script>
