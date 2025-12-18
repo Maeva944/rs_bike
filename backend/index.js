fc3599e64f1ef4f350bad86ab96b1092f21fcecf
AmaraABC <amara-noah.gakou@efrei.net>
2025-12-18 17:09:45 +0100

    updated file

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 const helmet = require('helmet');
+const rateLimit = require("express-rate-limit");
 const Inscription = require("./api/inscription");
 const Connexion = require('./api/auth');
 const Categorie = require('./api/categorie');
@@ -13,17 +14,19 @@ const message_contact = require('./api/message_contact');
 const reserver = require('./api/reserver');
 const creneaux = require('./api/creneaux');
 
+app.disable('x-powered-by');
+
 app.use(express.json());
 app.use(cors());
+app.use(helmet());
 app.use(
-helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: ["'self'"],
-      frameAncestors: ["'none'"],
-    },
-  })
-);
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 5,
+    message: { error: "Trop de tentatives. Réessayez plus tard." }
+  }));
 app.use(express.urlencoded({ extended: true }));
+
 
 app.use("/inscription", Inscription);
 app.use('/auth', Connexion);
